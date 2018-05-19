88b913e5f0a55defd0f701501ec2c218d4179a34
nabehide <iasoyv@gmail.com>
2018-05-19 15:14:07 +0900

    tweaks

--- pentagons/src/js/module/audio.js
+++ pentagons/src/js/module/audio.js
@@ -1,16 +1,11 @@
 export class Audio{
   constructor(webgl){
     this.webgl = webgl;
-    this.audioContext = (window.AudioContext) ? new AudioContext : new webkitAudioContext;
     this.render_random();
 
     this.loadAudio();
 
-    this.analyze = this.audioContext.createAnalyser();
-    this.analyze.fftSize = 2048;
     this.frequencyNum = 1024;
-
-    this.data = new Uint8Array(this.analyze.fftSize);
   }
 
   render_random(){
@@ -24,52 +19,47 @@ export class Audio{
   loadAudio(){
     const _this = this;
 
-    this.sendRequest();
-
     const btn = document.getElementById("soundIcon");
     btn.addEventListener("click", () => {
+
+      _this.audioContext = (window.AudioContext) ? new AudioContext : new webkitAudioContext;
+      _this.analyze = this.audioContext.createAnalyser();
+      _this.analyze.fftSize = 2048;
+      _this.data = new Uint8Array(this.analyze.fftSize);
+
       if (btn.classList.value == "fas fa-music fa-2x"){
         _this.source.stop();
-        _this.sendRequest();
         _this.render_random();
       }else{
-        _this.source.start(0);
+        const request = new XMLHttpRequest();
+        request.open("GET", "data/mrpu.m4a", true);
+        request.responseType = "arraybuffer"
 
-        (function animation(){
-          _this.analyze.getByteFrequencyData(_this.data);
+        request.onload = function(){
+          _this.audioContext.decodeAudioData(request.response, function(buffer){
+            if(_this.source){
+              _this.source.stop();
+            }
+            _this.source = _this.audioContext.createBufferSource();
+            _this.source.buffer = buffer;
+            _this.source.loop = true;
+            _this.source.connect(_this.analyze);
+            _this.source.connect(_this.audioContext.destination);
 
-          _this.webgl.render();
+            _this.source.start(0);
 
-          requestAnimationFrame(animation);
-        })();
+            (function animation(){
+              _this.analyze.getByteFrequencyData(_this.data);
+
+              _this.webgl.render();
+
+              requestAnimationFrame(animation);
+            })();
+
+          });
+        }.bind(_this);
+        request.send();
       }
     });
   };
-
-  sendRequest(){
-    const _this = this;
-    const request = new XMLHttpRequest();
-    request.open("GET", "data/mrpu.m4a", true);
-    request.responseType = "arraybuffer"
-
-    request.onload = function(){
-      _this.audioContext.decodeAudioData(request.response, function(buffer){
-        _this.connectNode(buffer);
-      });
-    }.bind(this);
-
-    request.send();
-
-  }
-
-  connectNode(buffer){
-    if(this.source){
-      this.source.stop();
-    }
-    this.source = this.audioContext.createBufferSource();
-    this.source.buffer = buffer;
-    this.source.loop = true;
-    this.source.connect(this.analyze);
-    this.source.connect(this.audioContext.destination);
-  }
 }
